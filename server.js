939be0d0495883733e0cbe23b43fb59bfdabae66
wbruns <wbruns07@gmail.com>
2022-03-03 20:54:42 -0600

    added front end files to the server and set up GET and POST requests so that the pages link and display data properly

--- server.js
+++ server.js
@@ -15,6 +15,8 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 // parse incoming JSON data
 app.use(express.json());
+// make the files in public available to the server
+app.use(express.static('public'));
 
 function filterByQuery(query, animalsArray) {
     let personalityTraitsArray = [];
@@ -116,6 +118,24 @@ app.post('/api/animals', (req, res) => {
         const animal = createNewAnimal(req.body, animals);
         res.json(animal);
     }
+});
+
+// routes for html files
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+app.get('/animals', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/animals.html'));
+});
+
+app.get('/zookeepers', (req, res) => {
+    res.sendFile(path.join(__dirname,'./public/zookeepers.html'));
+});
+
+// catch wildcard route requests
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
 });
 
 app.listen(PORT, () => {
